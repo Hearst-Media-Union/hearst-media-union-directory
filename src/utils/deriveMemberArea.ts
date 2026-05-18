618f2546a0989661393f9a75d4d2c032902c7a54
Andrew Berry <andberry89@gmail.com>
2026-05-17 23:24:03 -0400

    feat(directory): derive member representation areas

--- src/utils/deriveMemberArea.ts
+++ src/utils/deriveMemberArea.ts
@@ -4,55 +4,72 @@ type MemberAreaInput = {
 }
 
 const areaMappings: Record<string, Record<string, string>> = {
-  '320 West 57th Street': {
-    'Enthusiast Group': 'The Sheff - Floor 3',
-    Books: 'The Sheff - Floor 3',
-    'Best Products': 'The Sheff - Floor 3',
-    'Consumer Revenue': 'The Sheff - Floor 3',
-    Video: 'The Sheff - Floor 4',
-    'Lifestyle Group': 'The Sheff - Floor 4',
-    Delish: 'The Sheff - Floor 5',
-    'House Beautiful': 'The Sheff - Floor 5',
-  },
-  '300 West 57th Street': {
-    "Men's Health": 'New York - Floor 11',
-    "Women's Health": 'New York - Floor 11',
-    Visuals: 'New York - Floor 16',
-    'Lifestyle Group': 'New York - Floor 16',
-    Autos: 'New York - Floor 17',
-    'Car & Driver': 'New York - Floor 17',
-    'Food Network': 'New York - Floor 17',
-    HGTV: 'New York - Floor 17',
-    'Road & Track': 'New York - Floor 17',
-    'The Pioneer Woman': 'New York - Floor 17',
-    Cosmo: 'New York - Floor 22',
-    Video: 'New York - Floor 22',
-    Seventeen: 'New York - Floor 22',
-    Elle: 'New York - Floor 24',
-    'Oprah Daily': 'New York - Floor 24',
-    'Elle Decor': 'New York - Floor 25',
-    "Harper's Bazaar": 'New York - Floor 25',
-    'Town & Country': 'New York - Floor 25',
-    'Good Housekeeping Institute': 'New York - Floor 28/29',
-    Esquire: 'New York - Floor 28/29',
-    Prevention: 'New York - Floor 28/29',
-    'Good Housekeeping': 'New York - Floor 28/29',
-    'Edit Ops': 'New York - Floor 28/29',
-  },
-  'Birmingham, AL': {
-    '*': 'Birmingham, AL',
-  },
-  'El Segundo, CA': {
-    '*': 'El Segundo, CA',
-  },
-  'Easton, PA': {
-    '*': 'Easton, PA',
-  },
-  'Ann Arbor, MI': {
+  'Ann Arbor MI 1350 Eisenhower Place': {
     '*': 'Ann Arbor, MI',
   },
-  'Royal Oak, MI': {
+  'Ann Arbor MI 1585 Eisenhower Place': {
+    '*': 'Ann Arbor, MI',
+  },
+  'Birmingham AL 2901 Second Avenue South': {
+    '*': 'Birmingham, AL',
+  },
+  'Easton PA 132 S. 3rd Street': {
+    '*': 'Easton, PA',
+  },
+  'El Segundo CA 2151 East Grand Avenue': {
+    '*': 'El Segundo, CA',
+  },
+  'El Segundo CA 90245 2004-K E. Park Place': {
+    '*': 'El Segundo, CA',
+  },
+  'Los Angeles CA 6121 Santa Monica Blvd Suite 200': {
+    '*': 'El Segundo, CA',
+  },
+  'New York NY 300 West 57th Street': {
+    Autos: 'New York - Floor 17',
+    'Car & Driver': 'New York - Floor 17',
+    Cosmo: 'New York - Floor 22',
+    Elle: 'New York - Floor 24',
+    'Elle Décor': 'New York - Floor 25',
+    'Elle Decor': 'New York - Floor 25',
+    Esquire: 'New York - Floor 28/29',
+    'Food Network': 'New York - Floor 17',
+    'Good Housekeeping': 'New York - Floor 28/29',
+    'Good Housekeeping Institute': 'New York - Floor 28/29',
+    "Harper's Bazaar": 'New York - Floor 25',
+    HGTV: 'New York - Floor 17',
+    'Lifestyle Group': 'New York - Floor 16',
+    "Men's Health": 'New York - Floor 11',
+    'Oprah Daily': 'New York - Floor 24',
+    Prevention: 'New York - Floor 28/29',
+    Seventeen: 'New York - Floor 22',
+    'The Pioneer Woman': 'New York - Floor 17',
+    'Town & Country': 'New York - Floor 25',
+    Video: 'New York - Floor 22',
+    Visuals: 'New York - Floor 16',
+    "Women's Health": 'New York - Floor 11',
+  },
+  'New York NY 320 West 57th Street': {
+    'Best Products': 'The Sheff - Floor 3',
+    Books: 'The Sheff - Floor 3',
+    'Consumer Revenue': 'The Sheff - Floor 3',
+    Delish: 'The Sheff - Floor 5',
+    'Enthusiast Group': 'The Sheff - Floor 3',
+    'House Beautiful': 'The Sheff - Floor 5',
+    'Lifestyle Group': 'The Sheff - Floor 4',
+    Video: 'The Sheff - Floor 4',
+  },
+  'Royal Oak MI 120 E. Hudson': {
     '*': 'Royal Oak, MI',
+  },
+  'San Francisco CA 901 Mission Street': {
+    '*': 'El Segundo, CA',
+  },
+  'Santa Monica CA 3000 Ocean Park Blvd': {
+    '*': 'El Segundo, CA',
+  },
+  'Santa Monica CA 3420 Ocean Park Blvd': {
+    '*': 'El Segundo, CA',
   },
 }
 
